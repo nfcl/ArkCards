c404b2346f2d5378873ce48a4fabccbb90ca5d47
nfcl <2872786663@qq.com>
2022-10-29 19:51:06 +0800

    增加了SingleOperator类用于存储单个干员的信息

---
+++ Assets/scripts/GameScene/Operator/SingleOperator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+    namespace Operator
+    {
+        /// <summary>
+        /// 单个干员类
+        /// 存储了一个干员的信息
+        /// </summary>
+        public class SingleOperator
+        {
+
+        }
+    }
+}
