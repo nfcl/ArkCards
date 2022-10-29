7e656eff169d9eec5927272f66cb483207f4abf4
nfcl <2872786663@qq.com>
2022-10-29 19:51:28 +0800

    增加了SingleTeam类用于存储一支小队的信息

---
+++ Assets/scripts/GameScene/Team/SingleTeam.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+    namespace Team
+    {
+        /// <summary>
+        /// 单个队伍类
+        /// 存储了一支小队的信息
+        /// </summary>
+        public class SingleTeam
+        {
+
+        }
+    }
+}
