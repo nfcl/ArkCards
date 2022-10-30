a57aca71abb373216f8a8b625faa0922d7cf54bb
nfcl <2872786663@qq.com>
2022-10-30 09:23:42 +0800

    增加MapNodeState用于枚举地图节点类型

---
+++ Assets/scripts/GameScene/Map/MapNodeState.cs
@@ -0,0 +1,15 @@
+﻿namespace GameScene
+{
+    namespace Map
+    {
+        /// <summary>
+        /// 地图节点类型
+        /// </summary>
+        public enum MapNodeState 
+        {
+            Grass,      //草地
+            Water,      //水
+            Sand        //沙
+        }
+    }
+}
