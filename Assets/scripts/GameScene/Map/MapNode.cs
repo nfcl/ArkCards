aad1969c7c24118f46b22c4536f2a1bc7020d889
nfcl <2872786663@qq.com>
2022-10-30 09:24:17 +0800

    增加MapNode类用于保存单个地图节点信息

---
+++ Assets/scripts/GameScene/Map/MapNode.cs
@@ -0,0 +1,32 @@
+﻿namespace GameScene
+{
+    namespace Map
+    {
+        public class MapNode
+        {
+            /// <summary>
+            /// 节点类型
+            /// </summary>
+            private MapNodeState _state;    
+            /// <summary>
+            /// 节点位置
+            /// </summary>
+            private (int, int) _position;
+            /// <summary>
+            /// 节点是否还隐藏着
+            /// </summary>
+            private bool _isHide;
+
+            public MapNodeState State { get => _state; }
+            public (int, int) Position { get => _position; }
+            public bool IsHide { get => _isHide; }
+
+            public MapNode()
+            {
+                _state = MapNodeState.Grass;
+                _position= (0, 0);
+                _isHide = true;
+            }
+        }
+    }
+}
