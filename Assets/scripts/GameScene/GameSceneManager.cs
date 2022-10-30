e11ca345df8205a16a3c3da57c0572cde9ef394f
nfcl <2872786663@qq.com>
2022-10-30 09:26:50 +0800

    1.增加了string _teamName用于保存小队名称 2.增加了List<SingleOperator> _members用于保存小队成员信息 3.增加了string TeamName { get { return _teamName; } }用于访问小队名称

--- Assets/scripts/GameScene/GameSceneManager.cs
+++ Assets/scripts/GameScene/GameSceneManager.cs
@@ -1,23 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 namespace GameScene
 {
     public class GameSceneManager : MonoBehaviour
     {
-        private MapNodeDraw _mapNodeDrawControl;
+        private Map.MapNodeDraw _mapNodeDrawControl;
+        private Operator.OperatorCollection _operators;
+        private Team.TeamCollection _teams;
 
         public void Start()
         {
-            _mapNodeDrawControl = new MapNodeDraw(GameObject.Find("Map/Tilemap").GetComponent<Tilemap>());
-            _mapNodeDrawControl.AddTile("Grass",Resources.Load<Tile>("GameScene/MapNodes/TileMap/Grass"));
+            //地图初始化
+            MapInit();
+            //干员初始化
+            OperatorInit();
+            //小队初始化
+            TeamInit();
+        }
+
+        /// <summary>
+        /// 地图初始化
+        /// </summary>
+        public void MapInit()
+        {
+            _mapNodeDrawControl = new Map.MapNodeDraw(GameObject.Find("Map/Tilemap").GetComponent<UnityEngine.Tilemaps.Tilemap>());
+            _mapNodeDrawControl.AddTile("Grass", Resources.Load<UnityEngine.Tilemaps.Tile>("GameScene/MapNodes/TileMap/Grass"));
             _mapNodeDrawControl.Draw("Grass", (0, 0));
+            _mapNodeDrawControl.Draw("Grass", (1, 0));
+            _mapNodeDrawControl.Draw("Grass", (0, 2));
             _mapNodeDrawControl.Draw("Grass", (3, 0));
-            _mapNodeDrawControl.Draw("Grass", (0, 3));
-            _mapNodeDrawControl.Draw("Grass", (3, 3));
+            _mapNodeDrawControl.Draw("Grass", (0, 4));
+        }
+
+        /// <summary>
+        /// 干员信息初始化
+        /// </summary>
+        public void OperatorInit()
+        {
+
+        }
+
+        /// <summary>
+        /// 小队信息初始化
+        /// </summary>
+        public void TeamInit()
+        {
+
         }
     }
 }
-
