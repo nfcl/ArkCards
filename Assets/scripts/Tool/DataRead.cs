fb0833f77488a216d2b0a68a5c0eec9fbb2a83de
nfcl <2872786663@qq.com>
2022-12-02 11:46:48 +0800

    增加json读取静态方法

---
+++ Assets/scripts/Tool/DataRead.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Tool
+{
+    public static class DataRead
+    {
+        /// <summary>
+        /// 将path位置的json文件读取为json字符串并转换为json对象
+        /// </summary>
+        /// <param name="pathWithoutExtenion">读入的json文件地址</param>
+        public static T JsonReader<T>(string path)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(path);
+            string str = sr.ReadToEnd();
+            sr.Close();
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+    }
+}
